1df1dfbe06fb9d66c17785b8eb921fa6b9a1d727
slaven86 <slaven.lakota@gmail.com>
2022-03-23 13:47:09 +0100

    add test

--- app/test.py
+++ app/test.py
@@ -9,7 +9,7 @@ client = TestClient(app)
 
 
 def test_create_user():
-    data = {"first_name": "ivan", "last_name": "ivanovic", "email": "ivan@ivan.com", "username": "ivan123", "password": "ivan123"}
+    data = {"first_name": "Bojan", "last_name": "Bojanic", "email": "bojan@bojan.com", "username": "bojan123", "password": "bojan123"}
     response = client.post('/users', json.dumps(data))
     assert response.status_code == 201
     assert response.json() == {"msg": "New user created!"}
@@ -18,9 +18,9 @@ def test_create_user():
 
 def test_create_recipe():
 
-    data = {"name": "Cheese cake",
-            "description": "Maslac iseći na kockice, dodati plazmu i šećer u prahu, pa rukom sjediniti i formirati koricu u okruglom kalupu za torte. Umutiti slatku pavlaku, u drugoj posudi kašikom sjediniti Ella sir i šećer u prahu, zatim sve spojiti i mutiti mikserom, želatin rastopiti po uputstvu sa kesice i dodati u fil, muteći mikserom da se sve lepo sjedini, sipati preko korice.Voće, šećer i vodu staviti na šporet da provri, kuvati 5 minuta, skloniti za ringle, i onda dodati želatin u vrelu masu da se rastopi. Ostaviti da se prohladi, pa sipati preko fila."
-             }
+    data = {"name": "",
+            "description": ""
+            }
     token = ''
     headers = {"Authorization": f"Bearer {token}"}
     response = client.post('/recipes', json.dumps(data), headers=headers)
@@ -31,9 +31,9 @@ def test_create_recipe():
 def test_get_single_recipe():
     token = ''
     headers = {"Authorization": f"Bearer {token}"}
-    response = client.get('/recipes/37', headers=headers)
+    response = client.get('/recipes/1', headers=headers)
     assert response.status_code == 200
-    assert response.json()["name"] == "Palacinke"
+    assert response.json()["name"] == "Torta-Moskva"
 
 
 
@@ -56,8 +56,9 @@ def test_get_top_ingredients():
     assert response.status_code == 200
 
 
-
-
-
-
+def test_get_recipe_avg_rate():
+    token = ''
+    headers = {"Authorization": f"Bearer {token}"}
+    response = client.get('/recipes/1/rate', headers=headers)
+    assert response.status_code == 200
 
